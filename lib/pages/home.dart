82e8917c7cc35724538f082822c4a47890885331
Jafis <jafism2000@gmail.com>
2023-02-16 19:54:33 +0530

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,12 +7,52 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-         elevation: 0,
-         title: Text("Breakfast",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
-        centerTitle: true,
-        leading: Container(
+      appBar: buildAppBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40,left: 20,right: 20),
+            decoration: BoxDecoration(
+              boxShadow:[ BoxShadow(
+                color: Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: SvgPicture.asset("assets/icons/icons8-search ios.svg",height: 15,),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: SvgPicture.asset("assets/icons/settings-sliders.svg",height: 5,width: 5,),
+                ),
+                contentPadding: EdgeInsets.all(15),
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none
+                )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+       elevation: 0,
+       title: Text("Breakfast",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.bold),),
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: (){},
+        child: Container(
           margin: EdgeInsets.all(10),
           alignment: Alignment.center,
           child: SvgPicture.asset("assets/icons/angle-left.svg",
@@ -24,8 +64,11 @@ class HomePage extends StatelessWidget {
             borderRadius: BorderRadius.circular(10)
           ),
         ),
-        actions: [
-          Container(
+      ),
+      actions: [
+        GestureDetector(
+          onTap: (){},
+          child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
             width: 37,
@@ -38,8 +81,8 @@ class HomePage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(10)
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
